12d7c70fc5fff9d05685a5af31128cedd32e410f
diego.rosa <00007122@uca.edu.sv>
2025-11-23 21:29:45 -0600

    fix: agregar validacion de campos obligatorios

--- src/assets/components/Health/Card/InputsCard/InputsCard.jsx
+++ src/assets/components/Health/Card/InputsCard/InputsCard.jsx
@@ -45,80 +45,95 @@ const InputsCard = ({ bgIcon, icon, color, barColor, tittle, unit, minValue, mid
     };
 
     return (
-        <form className="p-3 w-80 h-cardHeight bg-cards-background flex flex-col justify-center items-center gap-2
+      <form
+        className="p-3 w-80 h-cardHeight bg-cards-background flex flex-col justify-center items-center gap-2
         rounded-lg shadow-lg shadow-black"
-            onSubmit={handleSubmit(onSubmit)}
-        >
-            <div className="px-2 w-full flex flex-row justify-left items-center gap-4">
-                <div className={`p-2 w-1/4 ${bgIcon} rounded-full flex flex-row justify-center items-center`}>
-                    <Icon icon={`${icon}`} width={50} height={50} color={`${color}`} />
-                </div>
-                <div className="w-3/4 flex flex-col justify-center items-left gap-2">
-                    <h4 className="w-full text-white text-Mtext">{tittle}</h4>
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="px-2 w-full flex flex-row justify-left items-center gap-4">
+          <div
+            className={`p-2 w-1/4 ${bgIcon} rounded-full flex flex-row justify-center items-center`}
+          >
+            <Icon icon={`${icon}`} width={50} height={50} color={`${color}`} />
+          </div>
+          <div className="w-3/4 flex flex-col justify-center items-left gap-2">
+            <h4 className="w-full text-white text-Mtext">{tittle}</h4>
 
-                    <div className='flex flex-row justify-start items-center gap-2'>
-                        <input type='text'{...register('values')}
-                            id='values'
-                            name='values'
-                            min={0}
-                            defaultValue={0}
-                            onBlur={getValue}
-                            className='p-1 w-4/5 bg-cards-background border-2 rounded-lg border-neutral-700 text-neutral-300
-                            placeholder:text-neutral-500 focus:border-neutral-500 focus:outline-none focus:text-neutral-300'
+            <div className="flex flex-row justify-start items-center gap-2">
+              <input
+                type="text"
+                {...register("values")}
+                id="values"
+                name="values"
+                required={true}
+                min={0}
+                defaultValue={null}
+                placeholder={0}
+                onBlur={getValue}
+                className="p-1 w-4/5 bg-cards-background border-2 rounded-lg border-neutral-700 text-neutral-300
+                            placeholder:text-neutral-500 focus:border-neutral-500 focus:outline-none focus:text-neutral-300"
+              />
+              <label htmlFor="values" className="text-lg text-white">
+                {unit}
+              </label>
+            </div>
+          </div>
+        </div>
 
-                        />
-                        <label htmlFor='values' className='text-lg text-white'>{unit}</label>
-                    </div>
+        <div className="px-3 w-full flex flex-col gap-1">
+          {/* TITULOS DE LA BARRA DE MEDICIÓN */}
+          <div className="px-1 w-full flex flex-row justify-end items-center gap-1">
+            <p className="text-white">{value}</p>
+            <p className="text-white">{unit}</p>
+          </div>
 
-                </div>
+          {/* BARRA DE MEDICIÓN */}
+          <div className="px-1 w-full flex flex-row justify-center gap-1">
+            {/* PRIMER RANGO */}
+            <div className={`h-5 grow rounded-l-lg ${bgIcon} overflow-hidden`}>
+              {/* BARRA DE COLOR */}
+              <div
+                style={{ width: `${(valueForMin / minValue) * 100}%` }}
+                className={`h-full ${barColor}`}
+              />
             </div>
 
-            <div className="px-3 w-full flex flex-col gap-1">
-
-                {/* TITULOS DE LA BARRA DE MEDICIÓN */}
-                <div className="px-1 w-full flex flex-row justify-end items-center gap-1">
-                    <p className="text-white">{value}</p>
-                    <p className="text-white">{unit}</p>
-                </div>
-
-                {/* BARRA DE MEDICIÓN */}
-                <div className='px-1 w-full flex flex-row justify-center gap-1'>
-
-                    {/* PRIMER RANGO */}
-                    <div className={`h-5 grow rounded-l-lg ${bgIcon} overflow-hidden`}>
-                        {/* BARRA DE COLOR */}
-                        <div style={{ width: `${(valueForMin / minValue) * 100}%` }}
-                            className={`h-full ${barColor}`} />
-                    </div>
-
-                    {/* SEGUNDO RANGO */}
-                    <div className={`h-5 grow ${bgIcon} overflow-hidden`}>
-                        <div style={{ width: `${(valueForMid / (midValue - minValue)) * 100}%` }}
-                            className={`h-full ${barColor}`} />
-                    </div>
-
-                    {/* TERCER RANGO */}
-                    <div className={`h-5 rounded-r-lg grow ${bgIcon} overflow-hidden`}>
-                        <div style={{ width: `${(valueForMax / (maxValue - midValue)) * 100}%` }}
-                            className={`h-full ${barColor}`} />
-                    </div>
-
-                </div>
-
+            {/* SEGUNDO RANGO */}
+            <div className={`h-5 grow ${bgIcon} overflow-hidden`}>
+              <div
+                style={{
+                  width: `${(valueForMid / (midValue - minValue)) * 100}%`,
+                }}
+                className={`h-full ${barColor}`}
+              />
             </div>
 
-            {/* INDICADORES PARA LA BARRA */}
-            <div className='pr-2 flex flex-row gap-14'>
-                <p className='grow text-white'>{minValue}</p>
-                <p className='grow text-white'>{midValue}</p>
+            {/* TERCER RANGO */}
+            <div className={`h-5 rounded-r-lg grow ${bgIcon} overflow-hidden`}>
+              <div
+                style={{
+                  width: `${(valueForMax / (maxValue - midValue)) * 100}%`,
+                }}
+                className={`h-full ${barColor}`}
+              />
             </div>
+          </div>
+        </div>
 
+        {/* INDICADORES PARA LA BARRA */}
+        <div className="pr-2 flex flex-row gap-14">
+          <p className="grow text-white">{minValue}</p>
+          <p className="grow text-white">{midValue}</p>
+        </div>
 
-            <div>
-                <input type="submit" className=" font-Montserrat py-2 px-4 border border-transparent text-lg font-medium rounded-lg text-white bg-background hover:bg-backgound" value={"Añadir"}/>
-            </div>
-
-        </form>
+        <div>
+          <input
+            type="submit"
+            className=" font-Montserrat py-2 px-4 border border-transparent text-lg font-medium rounded-lg text-white bg-background hover:bg-backgound"
+            value={"Añadir"}
+          />
+        </div>
+      </form>
     );
 }
 
